a10d7f793761516d87aa3067f1c4110b1a0bdb2d
sparky0520 <132695189+sparky0520@users.noreply.github.com>
2024-04-11 12:03:21 +0530

    b

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Header() {
     return (
         <nav className="flex justify-between my-5 text">
-            <Link to={'/'} className='text-2xl font-bold'>OnlyBlogs</Link>
+            <Link to={'/'} className='text-2xl font-bold'>Blogs</Link>
             <div className="flex gap-4">
                 <Link to={'/about'} className='text-2xl font-bold'>About</Link>
                 <Link to={'/contact'} className='text-2xl font-bold'>Contact</Link>
