5621c2bafb2180a03f76888410723441b1b5fb7e
Darsh Jain <132695189+sparky0520@users.noreply.github.com>
2024-04-12 17:20:57 +0530

    update

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,7 +4,7 @@ import './about.css'
 const About = () => {
   return (
     <>
-        <h1 className='text-3xl font-bold mx-5 my-20'>Hi, I am <span className='color-change'>Darsh</span></h1>
+        <h1 className='text-3xl font-bold mx-5 my-20'>Hi, I am <span className='color-change'>Darsh Jain</span></h1>
         <p className=' text-lg mx-5 my-20'>
           I am a passionate web developer and I am currently learning Japanese too. I am pursuing my B.Tech in Computer Science from G.B. Pant Engineering College.
         </p>
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact Page</div>
+    <>
+      <div className='text-3xl font-bold my-20'>You can reach out to me through</div>
+      <a href='mailto:darshjain05.in@gmail.com'>darshjain05.in@gmail.com</a> <br></br>
+      <a href='tel:+91 9958963296'>+91 9958963296</a><br></br>
+      <a href='https://www.linkedin.com/in/darsh-jain-521691218/' className='text-blue-900'>LinkedIn</a>
+    </>
   )
 }
 
