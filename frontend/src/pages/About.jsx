5f086f8e20b9d9ae289d68bfc313ed123e8987fb
Darsh Jain <132695189+sparky0520@users.noreply.github.com>
2024-04-12 07:47:51 +0530

    Update

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './about.css'
 
 const About = () => {
   return (
-    <div>About Page</div>
+    <>
+        <h1 className='text-3xl font-bold mx-5 my-20'>Hi, I am <span className='color-change'>Darsh</span></h1>
+        <p className=' text-lg mx-5 my-20'>
+          I am a passionate web developer and I am currently learning Japanese too. I am pursuing my B.Tech in Computer Science from G.B. Pant Engineering College.
+        </p>
+    </>
   )
 }
 
